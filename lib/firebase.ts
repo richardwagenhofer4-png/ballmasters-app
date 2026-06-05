1df8b2c52555a91713372e5f8a63b984f0a6bf68
richardwagenhofer4-png <richardwagenhofer4@gmail.com>
2026-06-05 14:55:36 -0400

    fix: set browserLocalPersistence for Firebase auth on iOS PWA

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,6 +13,7 @@ const firebaseConfig = {
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence).catch(() => {});
