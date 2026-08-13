8b88f87ad473f73f23a96c2db58d1f56f63af068
richardwagenhofer4-png <richardwagenhofer4@gmail.com>
2026-08-12 21:49:46 -0400

    Switch to production bundle ID com.ballmasters.app and LLC signing team

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ballmasters.app.dev',
+  appId: 'com.ballmasters.app',
   appName: 'Ball Masters',
   webDir: 'public',
   server: {
